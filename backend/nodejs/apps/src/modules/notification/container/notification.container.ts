b98459410ac6e1e395eb53fca37c393f6f2007a2
Anant Bisht <anant.b@1to10x.com>
2025-12-21 21:57:43 +0530

    Fix: Replace broken Inversify bindings with manual DI in notification.container.ts

--- backend/nodejs/apps/src/modules/notification/container/notification.container.ts
+++ backend/nodejs/apps/src/modules/notification/container/notification.container.ts
@@ -1,5 +1,6 @@
 import { Container } from 'inversify';
 import { NotificationService } from '../service/notification.service';
+import { Logger } from '../../../libs/services/logger.service';
 import { AuthTokenService } from '../../../libs/services/authtoken.service';
 import { AppConfig } from '../../tokens_manager/config/config';
 import { TYPES } from '../../../libs/types/container.types';
@@ -11,14 +12,24 @@ export class NotificationContainer {
 
   static async initialize(appConfig: AppConfig): Promise<Container> {
     const container = new Container();
+    const logger = new Logger({ service: 'NotificationService' });
+    container.bind<Logger>('Logger').toConstantValue(logger);
+
     const authTokenService = new AuthTokenService(
       appConfig.jwtSecret,
       appConfig.scopedJwtSecret,
     );
     container.bind<AuthTokenService>(TYPES.AuthTokenService).toConstantValue(authTokenService);
-    container.bind(NotificationService).toSelf().inSingletonScope();
-    container.bind(NotificationProducer).toSelf().inSingletonScope();
-    container.bind(NotificationConsumer).toSelf().inSingletonScope();
+
+    const notificationService = new NotificationService(authTokenService);
+    container.bind(NotificationService).toConstantValue(notificationService);
+
+    const notificationProducer = new NotificationProducer(appConfig.kafka, logger);
+    container.bind(NotificationProducer).toConstantValue(notificationProducer);
+
+    const notificationConsumer = new NotificationConsumer(appConfig.kafka, logger);
+    container.bind(NotificationConsumer).toConstantValue(notificationConsumer);
+
     this.container = container;
     return container;
   }
